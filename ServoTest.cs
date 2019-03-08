164fa8792745a00b9475ad25f8bf73712a1ffe9b
Juri Berlanda <juriberlanda@hotmail.com>
2019-03-08 09:43:36 +0100

    [InitialCommit] Add the APIs from vj-servo-controller.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

---
+++ ServoTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServoTest : MonoBehaviour {
+	private ServoControllerClient servoClient = null;
+
+	public long position = 1;
+
+	// Use this for initialization
+	void Start () {
+		servoClient = new ServoControllerClient();
+		servoClient.EventSetServoPosition(position);
+	}
+}
